d8f84161960314ff1a84ca5b96a9761d6de555bd
Abdullah BIYIK <abdullahb5355@gmail.com>
2023-02-09 21:29:20 +0300

    uselessadded

--- basic-practices/useless.exs
+++ basic-practices/useless.exs
@@ -1,1 +1,8 @@
 IO.puts("Hello World from Elixir")
+
+a = [1,2,3,3,4,4,4,5,6] -- [4,3,4,5,1]
+IO.puts(inspect(a))
+
+map = %{:foo => "bar"}
+keyword_lists = [foo: "bar", hello: "world"]
+tuples = {3.14, :pie, "Apple"}
