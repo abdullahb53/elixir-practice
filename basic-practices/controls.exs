2db02e47c238ff3bc2b93758f2accb86e4a3a5d0
Abdullah BIYIK <abdullahb5355@gmail.com>
2023-02-10 17:47:43 +0300

    controls-edited

--- basic-practices/controls.exs
+++ basic-practices/controls.exs
@@ -36,3 +36,54 @@ cond do
   true ->
     IO.puts("Catch all")
 end
+
+# with.
+user = %{first: "doomspork"} # error
+user = %{first: "Sean", last: "Callan"} # Callan, Sean
+
+with  {:ok, first} <- Map.fetch(user, :first),
+      {:ok, last} <- Map.fetch(user, :last) do
+      resp = last <> ", " <> first
+      IO.puts(resp)
+end
+
+# @@@@@@ Big "with" example without with!. @@@@@@
+# case Repo.insert(changeset) do
+#   {:ok, user} ->
+#     case Guardian.encode_and_sign(user, :token, claims) do
+#       {:ok, token, full_claims} ->
+#         important_stuff(token, full_claims)
+
+#       error ->
+#         error
+#     end
+
+#   error ->
+#     error
+# end
+#
+# @@@@@@@@@@@@@@@@ With "with"... @@@@@@@@@@@@@@@@
+#
+# with {:ok, user} <- Repo.insert(changeset),
+#      {:ok, token, full_claims} <- Guardian.encode_and_sign(user, :token, claims) do
+#   important_stuff(token, full_claims)
+# end
+
+# "with" supported else after 1.3
+
+import Integer
+
+m = %{a: 3, b: 44}
+
+a=
+  with  {:ok,number} <- Map.fetch(m,:c), true <- is_even(number) do
+        IO.puts "#{number} divided by 2 is #{div(number,2)}"
+        :even
+    else
+      :error ->
+        IO.puts("We don't have this item in map.")
+        :error
+      _ ->
+        IO.puts("It is odd")
+        :odd
+  end
