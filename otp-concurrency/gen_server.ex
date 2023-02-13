8c5ae533890992d0fb2c7a88d96e64ef51097af5
Abdullah BIYIK <abdullahb5355@gmail.com>
2023-02-14 00:14:26 +0300

    gen-server-added

---
+++ otp-concurrency/gen_server.ex
@@ -0,0 +1,16 @@
+defmodule SimpleQueue do
+  use GenServer
+
+  @doc """
+  Start our queue and link it.
+  This is a helper function
+  """
+  def start_link(state \\ []) do
+    GenServer.start_link(__MODULE__, state, name: __MODULE__)
+  end
+
+  @doc """
+  GenServer.init/1 callback
+  """
+  def init(state), do: {:ok, state}
+end
