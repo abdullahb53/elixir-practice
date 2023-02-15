81286d7c315428bcf6f6034e5af659ab5f2e9355
Abdullah BIYIK <abdullahb5355@gmail.com>
2023-02-16 02:08:42 +0300

    tesla-ageapi-added

---
+++ profiler/lib/age_api.ex
@@ -0,0 +1,11 @@
+defmodule Profiler.AgeApi do
+  use Tesla
+
+  plug(Tesla.Middleware.BaseUrl, "https://api.agify.io")
+  plug(Tesla.Middleware.JSON)
+
+  def age(name) do
+    get!("?name=" <> name)
+  end
+
+end
