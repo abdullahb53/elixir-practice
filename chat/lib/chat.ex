90167ddede079a9a3e6fcf7bce6acff6f2c7be61
Abdullah BIYIK <abdullahb5355@gmail.com>
2023-02-17 16:52:02 +0300

    chat-now-getting-env-val

--- chat/lib/chat.ex
+++ chat/lib/chat.ex
@@ -4,16 +4,25 @@ defmodule Chat do
   """
 
   @doc """
-  iex --sname kate@localhost -S mix
-  iex(kate@localhost)1> Chat.send_message(:alex@localhost, "how are you?")
+  # iex --sname kate@localhost -S mix
+  # iex(kate@localhost)1> Chat.send_message(:alex@localhost, "how are you?")
 
   """
   def receive_message(message) do
     IO.puts message
   end
 
+  def send_message(:moebi@localhost, message) do
+    spawn_task(__MODULE__, :receive_message_for_moebi, :moebi@localhost, [message, Node.self()])
+  end
+
   def send_message(recipient, message) do
     spawn_task(__MODULE__, :receive_message, recipient, [message])
+  end
+
+  def receive_message_for_moebi(message, from) do
+    IO.puts(message)
+    send_message(from, "chicken??bark! bark!")
   end
 
   def spawn_task(module, fun, recipient, args) do
@@ -24,7 +33,8 @@ defmodule Chat do
   end
 
   defp remote_supervisor(recipient) do
-    {Chat.TaskSupervisor, recipient}
+    # {Chat.TaskSupervisor, recipient}
+    Application.get_env(:chat, :remote_supervisor).(recipient)
   end
 
 end
