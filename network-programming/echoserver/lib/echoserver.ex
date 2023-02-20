06edea4650e9fe2bdc713672d17f99f7bc5e41da
Abdullah BIYIK <abdullahb5355@gmail.com>
2023-02-20 18:22:37 +0300

    network-programming-added

---
+++ network-programming/echoserver/lib/echoserver.ex
@@ -0,0 +1,18 @@
+defmodule Echoserver do
+  @moduledoc """
+  Documentation for `Echoserver`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Echoserver.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
