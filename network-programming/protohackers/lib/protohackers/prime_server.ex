5e8f38f9c4b13e5b1cd3abd438cfa0ef46d0b6b0
Abdullah BIYIK <abdullahb5355@gmail.com>
2023-02-22 02:34:33 +0300

    json-parse-for-is-prime-or-not

--- network-programming/protohackers/lib/protohackers/prime_server.ex
+++ network-programming/protohackers/lib/protohackers/prime_server.ex
@@ -17,11 +17,18 @@ defmodule Protohackers.PrimeServer do
       mode: :binary,
       active: false,
       reuseaddr: true,
-      exit_on_close: false
+      exit_on_close: false,
+      packet: :line,
+      # Pass the buffer size to options.
+      buffer: 1024 * 100
     ]
+
+
 
     case :gen_tcp.listen(5002, listen_options) do
       {:ok, listen_socket} ->
+        # Gets buffer size.
+        # dbg(:inet.getopts(listen_socket, [:buffer]))
         Logger.info("Starting prime server on port 5002")
         state = %__MODULE__{listen_socket: listen_socket, supervisor: supervisor}
         {:ok, state, {:continue, :accept}}
@@ -45,25 +52,54 @@ defmodule Protohackers.PrimeServer do
 
   # Helpers
   defp handle_connection(socket) do
-    case recv_until_closed(socket, _buffer = "", _buffered_size = 0) do
-      {:ok, data} ->
-        IO.puts("recv_unt socket buff @@@@@@@@@@@@@")
-        :gen_tcp.send(socket, data)
+    case echo_lines_until_closed(socket) do
+      :ok -> :ok
       {:error, reason} -> Logger.error("Failed to receive data: #{inspect(reason)}")
+      # {:error, reason} -> Logger.error("Failed to receive data: #{inspect(reason)}")
     end
 
     :gen_tcp.close(socket)
   end
 
-  @limit _100_kb = 1024 * 100
 
-  defp recv_until_closed(socket, buffer, buffered_size) do
+  defp echo_lines_until_closed(socket) do
      case :gen_tcp.recv(socket, 0, 10_1000) do
-      {:ok, data} when buffered_size + byte_size(data) > @limit -> {:error, :buffer_overflow}
-       {:ok, data} -> recv_until_closed(socket, [buffer, data], buffered_size + byte_size(data))
-       {:error, :closed} -> {:ok, buffer}
-       {:error, reason} -> {:ok, reason}
+      {:ok, data} ->
+        case Jason.decode(data) do
+          {:ok, %{"method" =>"isPrime", "number" => number}} when is_number(number) ->
+            Logger.debug("Received valid request for number: #{inspect(number)}")
+            response = %{"method" => "isPrime", "prime" => prime?(number)}
+            :gen_tcp.send(socket, [Jason.encode!(response), ?\n])
+            echo_lines_until_closed(socket)
+
+
+          # {:ok, json} ->
+          #   Logger.debug("Received data: #{inspect(json)}")
+          #   :gen_tcp.send(socket, [Jason.encode!(json), ?\n])
+          #   echo_lines_until_closed(socket)
+
+          other ->
+            Logger.debug("Received invalid request. Reason is: #{inspect(other)}")
+            :gen_tcp.send(socket, "malformed request@@@ \n")
+            {:error, :invalid_request}
+        end
+
+
+      {:error, :closed} ->
+        :ok
+
+      {:error, reason} ->
+        {:ok, reason}
      end
+  end
+
+  # Prime function.
+  defp prime?(number) when is_float(number), do: false
+  defp prime?(number) when number <= 1, do: false
+  defp prime?(number) when number in [2,3], do: true
+
+  defp prime?(number) do
+    not Enum.any?(2..trunc(:math.sqrt(number)), &(rem(number, &1) == 0))
   end
 
 end
